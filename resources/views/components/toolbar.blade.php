26a2a91b0aacf92fc5fe4c0a954a31e50560d22b
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2026-03-12 18:42:39 +0000

    hide tools in toolbar

--- resources/views/components/toolbar.blade.php
+++ resources/views/components/toolbar.blade.php
@@ -1,20 +1,15 @@
-@if(config('instruckt.enabled', false))
+@if(config('instruckt.enabled', true))
 <div id="instruckt-root">
+    @if(! config('instruckt.use_esm', true))
     <script>
         (function() {
+            var configEl = document.getElementById('instruckt-config');
+            var opts = configEl && configEl.textContent ? JSON.parse(configEl.textContent) : {};
+            window.__instrucktOpts = opts;
+
             function boot() {
                 if (window.__instruckt) return;
                 if (typeof Instruckt === 'undefined') return;
-                var opts = {
-                    endpoint: @json($endpoint),
-                    adapters: {!! $adapters !!},
-                    theme: @json($theme),
-                    position: @json($position),
-                };
-                var colors = {!! $colors !!};
-                var keys = {!! $keys !!};
-                if (Object.keys(colors).length) opts.colors = colors;
-                if (Object.keys(keys).length) opts.keys = keys;
                 window.__instruckt = Instruckt.init(opts);
             }
 
@@ -24,5 +19,15 @@
             document.getElementById('instruckt-root').appendChild(s);
         })();
     </script>
+    @else
+    <script>
+        (function() {
+            var configEl = document.getElementById('instruckt-config');
+            if (configEl && configEl.textContent) {
+                window.__instrucktOpts = JSON.parse(configEl.textContent);
+            }
+        })();
+    </script>
+    @endif
 </div>
 @endif
